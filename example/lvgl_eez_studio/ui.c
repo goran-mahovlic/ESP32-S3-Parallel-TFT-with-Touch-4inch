1276eb3a0929ffcc8cdb699054829f02f7cb190c
Goran Mahovlić <goran.mahovlic@gmail.com>
2024-02-25 16:46:06 +0100

    Adding eez-studio sample

---
+++ example/lvgl_eez_studio/ui.c
@@ -0,0 +1,37 @@
+#include "ui.h"
+#include "screens.h"
+#include "images.h"
+//#include "flow_def.h"
+
+static int16_t currentScreen = -1;
+
+static lv_obj_t *getLvglObjectFromIndex(int32_t index) {
+    if (index == -1) {
+        return 0;
+    }
+    return ((lv_obj_t **)&objects)[index];
+}
+
+static const void *getLvglImageByName(const char *name) {
+    for (size_t imageIndex = 0; imageIndex < sizeof(images) / sizeof(ext_img_desc_t); imageIndex++) {
+        if (strcmp(images[imageIndex].name, name) == 0) {
+            return images[imageIndex].img_dsc;
+        }
+    }
+    return 0;
+}
+
+void loadScreen(int index) {
+    currentScreen = index;
+    lv_obj_t *screen = getLvglObjectFromIndex(index);
+    lv_scr_load_anim(screen, LV_SCR_LOAD_ANIM_FADE_IN, 200, 0, false);
+}
+
+void ui_init() {
+    create_screens();
+    loadScreen(0);
+}
+
+void ui_tick() {
+    tick_screen(currentScreen);
+}
---
+++ example/lvgl_eez_studio/ui.h
@@ -0,0 +1,11 @@
+#ifndef EEZ_LVGL_UI_GUI_H
+#define EEZ_LVGL_UI_GUI_H
+
+#include "lvgl.h"
+
+void ui_init();
+void ui_tick();
+
+void loadScreen(int index);
+
+#endif // EEZ_LVGL_UI_GUI_H
